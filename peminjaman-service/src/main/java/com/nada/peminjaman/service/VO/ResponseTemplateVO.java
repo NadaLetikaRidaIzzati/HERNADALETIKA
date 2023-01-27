3c0bc8eba2f07f3dc6fa8594f89822c5440b256c
NadaLetikaRidaIzzati <113491775+NadaLetikaRidaIzzati@users.noreply.github.com>
2023-01-27 15:57:22 +0700

    Add files via upload

---
+++ peminjaman-service/src/main/java/com/nada/peminjaman/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nada.peminjaman.service.VO;
+
+
+
+import com.nada.peminjaman.service.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author nadaletika
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+    private Buku buku;
+}
