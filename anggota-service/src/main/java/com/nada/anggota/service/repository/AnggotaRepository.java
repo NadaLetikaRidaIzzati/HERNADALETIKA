d15616518abe5f7af6b69ca69eb1ec3a498384c2
NadaLetikaRidaIzzati <113491775+NadaLetikaRidaIzzati@users.noreply.github.com>
2023-01-27 15:49:45 +0700

    Add files via upload

---
+++ anggota-service/src/main/java/com/nada/anggota/service/repository/AnggotaRepository.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.nada.anggota.service.repository;
+
+
+import com.nada.anggota.service.entity.Anggota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author nadaletika
+ */
+@Repository
+public interface AnggotaRepository extends JpaRepository<Anggota, Long> {
+
+    public Anggota findByAnggotaId(Long anggotaId);
+    
+}
